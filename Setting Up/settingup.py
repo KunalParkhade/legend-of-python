80dee9de9f0d6bfc1cfbd72c0b7a9d22e57f0d85
Kunal Parkhade <113717557+KunalParkhade@users.noreply.github.com>
2024-02-26 12:57:55 +0000

    Until Control Flow Module updated

---
+++ Setting Up/settingup.py
@@ -0,0 +1,6 @@
+#Welcome to the first chapter of The Legend of Python! 🐍
+
+#The programming language we are learning is called Python, created by a developer named Guido van Rossum in the early 90s.
+
+# Write code below 💖
+print('Hi')
